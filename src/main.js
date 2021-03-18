8e567151824f6b5cbd1900e7baeca309380c1b87
Kishan Ranoliya <kishanranoliya59@gmail.com>
2021-03-18 15:40:44 +0530

    Minor changes

--- src/main.js
+++ src/main.js
@@ -4,50 +4,7 @@ import vuetify from './plugins/vuetify';
 import './registerServiceWorker'
 import firebase from "firebase";
 
-var config = {
-  apiKey: "AIzaSyCZ96GQZonhNAxFYf6D1467O2lSgr7uess",
-  authDomain: "trydemo-notification-2021.firebaseapp.com",
-  projectId: "trydemo-notification-2021",
-  storageBucket: "trydemo-notification-2021.appspot.com",
-  messagingSenderId: "137013705214",
-  appId: "1:137013705214:web:797bb6b61709cc95e328af"
-};
-const app = firebase.initializeApp(config);
-console.log(app)
-const messaging = firebase.messaging();
 
-// navigator.serviceWorker.register('../public/firebase-messaging-sw')
-// .then((registration) => {
-//   messaging.useServiceWorker(registration);
-
-//   // Request permission and get token.....
-// });
-
-messaging.usePublicVapidKey("BJpa5BUXJgyM_7c0YwagVRI1to8IH1xY3xcAPB-26LnnDTeG_B1VeF88wrzAJIZQarnZa3ek51FHC39HWqsiByU"); // 1. Generate a new key pair
-
-messaging.getToken({ vapidKey: 'BJpa5BUXJgyM_7c0YwagVRI1to8IH1xY3xcAPB-26LnnDTeG_B1VeF88wrzAJIZQarnZa3ek51FHC39HWqsiByU' }).then((currentToken) => {
-  console.log(currentToken)
-  if (currentToken) {
-
-  } else {
-    console.log('No registration token available. Request permission to generate one.');
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  // ...
-});
-
-// Request Permission of Notifications
-messaging.requestPermission().then(() => {
-  console.log('Notification permission granted.');
-
-  // Get Token
-  messaging.getToken().then((token) => {
-    console.log(token)
-  })
-}).catch((err) => {
-  console.log('Unable to get permission to notify.', err);
-});
 Vue.config.productionTip = false
 
 new Vue({
