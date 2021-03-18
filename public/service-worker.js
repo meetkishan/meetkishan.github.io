6410d37277f6bbbe3a8eb147991ee1a97173eee2
Kishan Ranoliya <kishanranoliya59@gmail.com>
2021-03-18 13:46:37 +0530

    service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,65 @@
 // Implementing pusher beams ref: https://pusher.com/docs/beams/getting-started/web/sdk-integration
 // importScripts("https://js.pusher.com/beams/service-worker.js");
 
+// If you are using 'GenerateSW' (default) for your workboxPluginMode setting this file is auto generated for you.
+// If you are using 'InjectManifest' then add this to your custom SW after your standard workbox config
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+// firebase.initializeApp({
+//   'messagingSenderId': '137013705214' // 4. Get Firebase Configuration
+// });
+import firebase from "firebase";
 
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
 
+navigator.serviceWorker.register('service-worker.js')
+.then((registration) => {
+  messaging.useServiceWorker(registration);
+
+  // Request permission and get token.....
+});
+
+var config = {
+    apiKey: "AIzaSyCZ96GQZonhNAxFYf6D1467O2lSgr7uess",
+    authDomain: "trydemo-notification-2021.firebaseapp.com",
+    projectId: "trydemo-notification-2021",
+    storageBucket: "trydemo-notification-2021.appspot.com",
+    messagingSenderId: "137013705214",
+    appId: "1:137013705214:web:797bb6b61709cc95e328af"
+  };
+  const app = firebase.initializeApp(config);
+  console.log(app)
+  const messaging = firebase.messaging();
+  
+  
+  
+  messaging.usePublicVapidKey("BJpa5BUXJgyM_7c0YwagVRI1to8IH1xY3xcAPB-26LnnDTeG_B1VeF88wrzAJIZQarnZa3ek51FHC39HWqsiByU"); // 1. Generate a new key pair
+  
+  messaging.getToken({ vapidKey: 'BJpa5BUXJgyM_7c0YwagVRI1to8IH1xY3xcAPB-26LnnDTeG_B1VeF88wrzAJIZQarnZa3ek51FHC39HWqsiByU' }).then((currentToken) => {
+    console.log(currentToken)
+    if (currentToken) {
+  
+    } else {
+      console.log('No registration token available. Request permission to generate one.');
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+  });
+  
+  // Request Permission of Notifications
+  messaging.requestPermission().then(() => {
+    console.log('Notification permission granted.');
+  
+    // Get Token
+    messaging.getToken().then((token) => {
+      console.log(token)
+    })
+  }).catch((err) => {
+    console.log('Unable to get permission to notify.', err);
+  });
 // For app update 
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
