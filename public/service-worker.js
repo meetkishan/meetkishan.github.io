c637d3087ff89af11ab9c88f680546bd8b0c755a
Kishan Ranoliya <kishanranoliya59@gmail.com>
2021-03-18 10:52:10 +0530

    Firebase added

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,17 @@
 
 // If you are using 'GenerateSW' (default) for your workboxPluginMode setting this file is auto generated for you.
 // If you are using 'InjectManifest' then add this to your custom SW after your standard workbox config
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+firebase.initializeApp({
+  'messagingSenderId': '137013705214' // 4. Get Firebase Configuration
+});
 
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
+
+// For app update 
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
         self.skipWaiting()
--- src/main.js
+++ src/main.js
@@ -2,7 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import './registerServiceWorker'
+import * as firebase from "firebase";
 
+var config = {
+  apiKey: "AIzaSyCZ96GQZonhNAxFYf6D1467O2lSgr7uess",
+  authDomain: "trydemo-notification-2021.firebaseapp.com",
+  projectId: "trydemo-notification-2021",
+  storageBucket: "trydemo-notification-2021.appspot.com",
+  messagingSenderId: "137013705214",
+  appId: "1:137013705214:web:797bb6b61709cc95e328af"
+};
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+
+messaging.usePublicVapidKey("BJpa5BUXJgyM_7c0YwagVRI1to8IH1xY3xcAPB-26LnnDTeG_B1VeF88wrzAJIZQarnZa3ek51FHC39HWqsiByU"); // 1. Generate a new key pair
+
+// Request Permission of Notifications
+messaging.requestPermission().then(() => {
+  console.log('Notification permission granted.');
+
+  // Get Token
+  messaging.getToken().then((token) => {
+    console.log(token)
+  })
+}).catch((err) => {
+  console.log('Unable to get permission to notify.', err);
+});
 Vue.config.productionTip = false
 
 new Vue({
